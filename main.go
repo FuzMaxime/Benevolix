c346fa3e699a7f5c4d6791974a005af3c3c8a898
Harcadakad <evan.guillet@ynov.com>
2024-12-05 14:24:52 +0100

    :sparkles: feature(user): adds routes from main

--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"benevolix/config"
+	"benevolix/pkg/user"
+	"log"
+	"net/http"
 
 	"github.com/go-chi/chi"
 )
 
 func Routes(configuration *config.Config) *chi.Mux {
 	router := chi.NewRouter()
-	// TODO : replace by valid routes
-	// router.Mount("/api/v1/visit", visit.Routes(configuration))
-	// router.Mount("/api/v1/cat", cat.Routes(configuration))
-	// router.Mount("/api/v1/treatment", treatment.Routes(configuration))
-	// router.Mount("/api/v1/agecalculator", agecalculator.Routes(configuration))
-	// router.Mount("/api/v1/soundidentifier", soundidentifier.Routes(configuration))
+
+	router.Mount("/user", user.Routes(configuration))
 	return router
+}
+
+func main() {
+	// Initialisation de la configuration
+	configuration, err := config.New()
+	if err != nil {
+		log.Panicln("Configuration error:", err)
+	}
+
+	// Initialisation des routes
+	router := Routes(configuration)
+
+	log.Println("Serving on :8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
