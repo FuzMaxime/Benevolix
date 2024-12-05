391cfa0fae611058472dd3c0bb5c55df9827f785
Harcadakad <evan.guillet@ynov.com>
2024-12-05 14:37:14 +0100

    :sparkles: feature(user): adds model for user

--- pkg/model/user.go
+++ pkg/model/user.go
@@ -6,7 +6,7 @@ import (
 )
 
 type UserRequest struct {
-	Name      string `json:"name"`
+	LastName  string `json:"last_name"`
 	FirstName string `json:"first_name"`
 	Email     string `json:"email"`
 	Password  string `json:"password"`
@@ -15,15 +15,30 @@ type UserRequest struct {
 }
 
 func (a *UserRequest) Bind(r *http.Request) error {
-	if a.Name == "" {
-		return errors.New("name must be there")
+	if a.LastName == "" {
+		return errors.New("lastname must be there")
+	}
+	if a.FirstName == "" {
+		return errors.New("firstname must be there")
+	}
+	if a.Email == "" {
+		return errors.New("email must be there")
+	}
+	if a.Password == "" {
+		return errors.New("password must be there")
+	}
+	if a.City == "" {
+		return errors.New("city must be there")
+	}
+	if a.Bio == "" {
+		return errors.New("bio must be there")
 	}
 	// TODO : Implement test here
 	return nil
 }
 
 type UserResponse struct {
-	Name      string        `json:"name"`
+	LastName  string        `json:"last_name"`
 	FirstName string        `json:"first_name"`
 	Email     string        `json:"email"`
 	Password  string        `json:"password"`
